72a7f8756d5e95d870514059eea88e037e3a807c
Jingwei Chen <jc382@outlook.com>
2020-05-28 15:26:45 -0400

    create spring project

---
+++ PPMToolJava/src/test/java/dev/jingweichen/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jingweichen.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
